186279c8f5fbb53d03fc8bb8f67b8c51547f4228
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2019-12-21 22:29:45 +0700

    membuat activity_login

--- app/src/main/java/com/example/uasmobile/activities/LoginActivity.java
+++ app/src/main/java/com/example/uasmobile/activities/LoginActivity.java
@@ -1,4 +1,23 @@
 package com.example.uasmobile.activities;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+
+import com.example.uasmobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void handlerkembali(View view) {
+        finish();
+    }
 }
